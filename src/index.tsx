ea00fc61cc4c9e8a8946ccdbd9a303a0b8bb5e12
Ricardo Souza <ricardo.souza@deliveryhero.com>
2020-11-18 02:13:58 +0100

    Define base style components

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { Content, Title } from './components'
 import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World!</div>
+    <Content>
+      <Title>Sudoku</Title>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
