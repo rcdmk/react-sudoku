3305bc26a327b9ac69a8b9f4229cf37653a01eac
Ricardo Souza <ricardo.souza@deliveryhero.com>
2020-11-22 00:00:05 +0100

    Change font weight when number is not part of the puzzle

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -13,12 +13,16 @@ interface Props {
 
 interface State {
   isActive: boolean
+  isPuzzle: boolean
   value: N
 }
 
 const Block: FC<Props> = ({ colIndex, rowIndex }) => {
-  const state = useSelector<Reducer, State>(({ workingGrid, selectedBlock }) => ({
-    isActive: selectedBlock ? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex : false,
+  const state = useSelector<Reducer, State>(({ puzzleGrid, selectedBlock, workingGrid }) => ({
+    isActive: selectedBlock
+      ? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex
+      : false,
+    isPuzzle: puzzleGrid ? puzzleGrid[rowIndex][colIndex] !== 0 : false,
     value: workingGrid ? workingGrid[rowIndex][colIndex] : 0,
   }))
 
@@ -29,7 +33,12 @@ const Block: FC<Props> = ({ colIndex, rowIndex }) => {
   }
 
   return (
-    <Container active={state.isActive} data-cy={`block-${rowIndex}-${colIndex}`} onClick={handleClick}>
+    <Container
+      active={state.isActive}
+      data-cy={`block-${rowIndex}-${colIndex}`}
+      onClick={handleClick}
+      puzzle={state.isPuzzle}
+    >
       {state.value || ''}
     </Container>
   )
