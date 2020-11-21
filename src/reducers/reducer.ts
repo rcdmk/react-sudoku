409a25b92c3da147ed7aa465bf0c98cc5a15c309
Ricardo Souza <ricardo.souza@deliveryhero.com>
2020-11-21 06:01:06 +0100

    Add default reducer

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,9 +1,19 @@
 import { AnyAction } from 'redux'
 
+import { createFullGrid } from 'utils'
+
+import { Reducer } from './interfaces'
+import * as types from './types'
+
 const initialState = {}
 
-function reducer(state = initialState, action: AnyAction) {
+function reducer(state = initialState, action: AnyAction): Reducer {
   switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        grid: createFullGrid(),
+      }
     default:
       return state
   }
