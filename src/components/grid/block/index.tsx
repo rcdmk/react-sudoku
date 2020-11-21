46c4482f9880abbfb3cc5ec1559cfcb9b189d8d7
Ricardo Souza <ricardo.souza@deliveryhero.com>
2020-11-21 18:03:15 +0100

    Add keyboard navigation

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, FC } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction } from 'redux'
 
-import { Reducer, select } from 'reducers'
+import { Reducer, selectBlock } from 'reducers'
 import { INDEX, N } from 'typings'
 import { Container } from './styles'
 
@@ -27,7 +27,7 @@ const Block: FC<Props> = ({ colIndex, rowIndex }) => {
   const dispatch = useDispatch<Dispatch<AnyAction>>()
 
   const handleClick = () => {
-    if (!state.isActive) dispatch(select([rowIndex, colIndex]))
+    if (!state.isActive) dispatch(selectBlock([rowIndex, colIndex]))
   }
 
   return (
