e336698811e99aa5456cf3ca724dc1e6fe1ca2ec
Ricardo Souza <ricardo.souza@deliveryhero.com>
2020-11-24 22:43:11 +0100

    Add notification actions

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction } from 'redux'
 
 import { createGrid, fillBlock, Reducer, selectBlock } from 'reducers'
-import { BLOCK_COORDS, GRID, INDEX, N, NUMBERS } from 'typings'
+import { BLOCK_COORDS, GRID, INDEX, N } from 'typings'
 
 import Block from './block'
 import { Container, Row } from './styles'
